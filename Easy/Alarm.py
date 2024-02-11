db7c74c094d3062d7053f9315c4990a6cecd22b0
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-11 11:17:17 +0545

    Aalarm

---
+++ Easy/Alarm.py
@@ -0,0 +1,25 @@
+from playsound import playsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def alarm(seconds):
+    time_elaplsed = 0
+    print(CLEAR)
+    while time_elaplsed < seconds:
+        time.sleep(1)
+        time_elaplsed +=1
+        
+        time_left = seconds - time_elaplsed
+        minutes_left =time_left // 60
+        seconds_left = time_left % 60
+        
+        print(f"{CLEAR_AND_RETURN}The alarm will sound in:{minutes_left:02d}:{seconds_left:02d}")
+    playsound(r"C:\Users\Hp\Downloads\PythonP\Easy\alarm.wav")
+
+minutes = int(input("Enter minutes:"))
+seconds = int(input("Enter Seconds:"))
+total = minutes*60+seconds
+        
+alarm(total)
