a3b2bd0efd5a0b38fd6834686984af9c1cf401c1
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-13 11:41:17 +0545

    PTP

--- Easy/NumberGuesser.py
+++ Easy/NumberGuesser.py
@@ -1,8 +1,15 @@
 import random
 
 range_input = int(input("Type a number:\n"))
-
+guesses = 0
 random_number = random.randint(0, range_input)
 
 while True:
+    guesses+=1
     user_guess = int(input("Make a guess:\n"))
+    if range_input == user_guess:
+        print("Yay!! It matches")
+        break
+else:
+    print("Boo!!! Try again")
+print("You got it in",guesses,"guesses")
