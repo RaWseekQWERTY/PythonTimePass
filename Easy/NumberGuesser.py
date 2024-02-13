1eb574208b333c09a0e547e0c2d58b8aa18b65b1
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-13 11:44:23 +0545

    PTP

--- Easy/NumberGuesser.py
+++ Easy/NumberGuesser.py
@@ -1,15 +1,28 @@
 import random
+CLEAR = "\033[2J"
+while True:
+    print(CLEAR)
+    try:
+        range_input = int(input("Type a number:\n"))
+        break
+    except ValueError:
+        print("Please enter a valid number.")
 
-range_input = int(input("Type a number:\n"))
 guesses = 0
 random_number = random.randint(0, range_input)
 
 while True:
-    guesses+=1
-    user_guess = int(input("Make a guess:\n"))
-    if range_input == user_guess:
+    guesses += 1
+    try:
+        user_guess = int(input("Make a guess:\n"))
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+    
+    if random_number == user_guess:
         print("Yay!! It matches")
         break
-else:
-    print("Boo!!! Try again")
-print("You got it in",guesses,"guesses")
+    else:
+        print("Boo!!! Try again")
+
+print("You got it in", guesses, "guesses")
