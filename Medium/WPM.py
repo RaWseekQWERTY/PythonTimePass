4bcd57be2292f93e0791f088e4c13d3b3453fc68
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-18 12:11:07 +0545

    WPM

--- Medium/WPM.py
+++ Medium/WPM.py
@@ -2,8 +2,10 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_WHITE)
     stdscr.clear()
-    stdscr.addstr("hello world")
+    stdscr.addstr("hello world", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
     
