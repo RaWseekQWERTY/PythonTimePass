e75500ae5bdcc86e3589c5e5f734e2d1bd910095
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-20 13:16:23 +0545

    WPM

--- Medium/WPM.py
+++ Medium/WPM.py
@@ -5,7 +5,7 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("hello world", curses.color_pair(1))
+    stdscr.addstr("Welcome to WPM.exe! Test your limits today", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
     
