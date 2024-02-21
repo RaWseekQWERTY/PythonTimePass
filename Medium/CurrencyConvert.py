874f9e597e9ed309d3781d8467a97c053a58d8d2
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-21 17:16:55 +0545

    CC

--- Medium/CurrencyConvert.py
+++ Medium/CurrencyConvert.py
@@ -3,3 +3,13 @@ from pprint import PrettyPrinter
 
 BASE_URL = "https://free.currconv.com/"
 API_KEY = "190c0d3122ef7a2c943c" #freecurrencyconverterapi.com
+
+printer = PrettyPrinter()
+
+def get_currency():
+    endpoint = f"api/v7/currencies?apikey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()
+    printer.pprint(data)
+    
+get_currency()
