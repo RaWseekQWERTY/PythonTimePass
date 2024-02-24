0b3bb78b1c13d5da41f39b0fd905b87e31fd977e
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-24 19:32:37 +0545

    tryCatch

--- Medium/CurrencyConvert.py
+++ Medium/CurrencyConvert.py
@@ -7,13 +7,21 @@ API_KEY = "190c0d3122ef7a2c943c" #freecurrencyconverterapi.com
 printer = PrettyPrinter()
 
 def get_currency():
-    endpoint = f"api/v7/currencies?apiKey={API_KEY}" # ?(query)
+    endpoint = f"/api/v7/currencies?apiKey={API_KEY}"
     url = BASE_URL + endpoint
-    data = get(url).json()
-    
-    data = list(data.items())
-    data.sort()
-    return data
-    
+    try:
+        response = get(url)
+        response.raise_for_status()  # Raise an exception for bad status codes
+        data = response.json()['results']
+        data = list(data.items())
+        data.sort()
+        return data
+    except Exception as e:
+        print("Error occurred:", e)
+        return None
+
 data = get_currency()
-printer.pprint(data)
+if data is not None:
+    printer.pprint(data)
+else:
+    print("Failed to fetch currency data.")
