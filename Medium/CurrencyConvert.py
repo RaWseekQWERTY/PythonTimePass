95ba4d32d0b7a79f2db7e71560174d97f949f35b
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-02-28 20:17:30 +0545

    final

--- Medium/CurrencyConvert.py
+++ Medium/CurrencyConvert.py
@@ -2,7 +2,7 @@ from requests import get
 from pprint import PrettyPrinter
 
 BASE_URL = "https://free.currconv.com"
-API_KEY = "190c0d3122ef7a2c943c" #freecurrencyconverterapi.com
+API_KEY = "YOUR_API_KEY" #freecurrencyconverterapi.com
 
 printer = PrettyPrinter()
 
